052d8723ad46d640f94785775a42cc597ae27977
somya-jangid <somyajangid78@gmail.com>
2025-02-24 02:18:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import React from 'react';import ReactDOM from 'react-dom';import './styles.css';import App from './App';ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
+import './styles.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
